b4424ff51b14fe373189a322840c5c96970749f6
KEINOS <github@keinos.com>
2021-07-17 16:46:17 +0900

    feat: image info output

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # VSCode-Dev-Container-Go
 
-VSCode の Remote-Containers で Golang 開発をするためのコンテナ・イメージ（Alpine ベース）
+VSCode の Remote-Containers で Golang 開発をするための Docker コンテナに使うベース・イメージです。（Go 1.16, Debian ベース）
+
+- @KEINOS が個人的に開発時に良く使う Go や apt などのパッケージを予めインストールしています。（ARM64, intel x86_64互換向け）
+  - インストール済みのパッケージ情報: [image_info.txt](image_info.txt)
+- 常に最新の状態で開発するため、毎週月曜日にイメージを自動ビルドしています。そのため、開発ツールを利用するしたバージョン依存する
--- update-info.sh
+++ update-info.sh
@@ -3,25 +3,19 @@
 #  イメージに含まれているパッケージなどの情報をファイルに出力します。
 # =============================================================================
 
+set -eu
+
 NAME_TAG_IMG='test:local'
 NAME_FILE_INFO='image_info.txt'
 PATH_FILE_INFO="./${NAME_FILE_INFO}"
 
-docker build -t "$NAME_TAG_IMG" .
+docker build -t "$NAME_TAG_IMG" . || {
+    echo >&2 "Error during building image"
+    exit 1
+}
 
-# 初期化
-rm -f "$PATH_FILE_INFO"
-
-result=$(docker run --rm "$NAME_TAG_IMG" /bin/bash -c 'ls -1A "/go/bin"')
-echo '===============================================================================' >> "$PATH_FILE_INFO"
-echo ' Installed go packages' >> "$PATH_FILE_INFO"
-echo '===============================================================================' >> "$PATH_FILE_INFO"
-echo "$result" >> "$PATH_FILE_INFO"
-echo >> "$PATH_FILE_INFO"
-
-result=$(docker run --rm "$NAME_TAG_IMG" /bin/bash -c "sudo dpkg -l")
-echo '===============================================================================' >> "$PATH_FILE_INFO"
-echo ' Installed apt packages' >> "$PATH_FILE_INFO"
-echo '===============================================================================' >> "$PATH_FILE_INFO"
-echo "$result" | tail -n +4 >> "$PATH_FILE_INFO"
-echo >> "$PATH_FILE_INFO"
+result=$(docker run --rm -v "$(pwd):/root" --user root --workdir /root "$NAME_TAG_IMG" ./print_info_image.sh 2>&1) || {
+    echo >&2 "Error during running image"
+    exit 1
+}
+echo "$result" > "$PATH_FILE_INFO"
