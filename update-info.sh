1742ca3b97f6c81e5e2ee915cb5ed222e4a093b6
KEINOS <github@keinos.com>
2021-07-18 21:20:06 +0900

    Feat shellcheck shellspec (PR #3)
    * feat: shellcheck and shellspec
    * Merge "print_info_image.sh" to "update-info.sh"
    * chore: add editorconfig

--- update-info.sh
+++ update-info.sh
@@ -3,19 +3,73 @@
 #  イメージに含まれているパッケージなどの情報をファイルに出力します。
 # =============================================================================
 
-set -eu
-
 NAME_TAG_IMG='test:local'
 NAME_FILE_INFO='image_info.txt'
 PATH_FILE_INFO="./${NAME_FILE_INFO}"
+PATH_FILE_SCRIPT='./update-info.sh'
 
-docker build -t "$NAME_TAG_IMG" . || {
-    echo >&2 "Error during building image"
+# -----------------------------------------------------------------------------
+#  コンテナをビルドして、コンテナ内でこのファイルを実行し、結果をファイルに保存します。
+# -----------------------------------------------------------------------------
+
+if [ ! -r "/.dockerenv" ]; then
+    set -eu
+
+    docker build -t "$NAME_TAG_IMG" . || {
+        echo >&2 "Error during building image"
+
+        exit 1
+    }
+
+    result=$(docker run --rm -v "$(pwd):/root" --user root --workdir /root "$NAME_TAG_IMG" "$PATH_FILE_SCRIPT" 2>&1) || {
+        echo >&2 "$result"
+        echo >&2 "Error during running image"
+
+        exit 1
+    }
+    echo "$result" >"$PATH_FILE_INFO"
+
+    exit 0
+fi
+
+# -----------------------------------------------------------------------------
+#  コンテナ内でこのスクリプトが実行された場合に実行されるコードです。
+# -----------------------------------------------------------------------------
+result=$(shfmt -d "$PATH_FILE_SCRIPT") || {
+    echo >&2 "Error during running shfmt"
+    echo >&2 "$result"
+
     exit 1
 }
 
-result=$(docker run --rm -v "$(pwd):/root" --user root --workdir /root "$NAME_TAG_IMG" ./print_info_image.sh 2>&1) || {
-    echo >&2 "Error during running image"
+result=$(shellcheck --shell=sh "$PATH_FILE_SCRIPT") || {
+    echo >&2 "Error during running shellcheck"
+    echo >&2 "$result"
+
     exit 1
 }
-echo "$result" > "$PATH_FILE_INFO"
+
+echo '==============================================================================='
+echo ' OS Info'
+echo '==============================================================================='
+cat /etc/os-release
+
+echo '==============================================================================='
+echo ' Program Lang Info'
+echo '==============================================================================='
+go version
+python --version
+python3 --version
+printf "%s %s\n" "node.js" "$(node --version)"
+printf "%s %s\n" "Perl" "$(perl --version | head -n 3 | grep -o -E "(v[0-9]+\.){1}[0-9]+(\.[0-9]+)?" | head -n1)"
+
+echo '==============================================================================='
+echo ' Installed Go packages'
+echo '==============================================================================='
+ls -1A "/go/bin"
+
+echo '==============================================================================='
+echo ' Installed apt packages'
+echo '==============================================================================='
+result="$(sudo dpkg -l)"
+echo "$result" | tail -n +4
