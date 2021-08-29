e564e4dde92a47b621e9d299b06ac1338f9e90fb
KEINOS <github@keinos.com>
2021-08-29 19:35:23 +0900

    fix: issue #35 better "run-shellcheck" alias definition

--- embedded_sh/aliases.sh
+++ embedded_sh/aliases.sh
@@ -5,6 +5,6 @@
 alias welcome='/usr/local/bin/welcome_msg.sh'
 alias run-shfmt='shfmt -d -i=4 -ln=posix'
 alias run-coverage='go-carpet -256colors -include-vendor'
-alias run-shellcheck='shellcheck -a --shell=sh'
+alias run-shellcheck='shfmt -f . | xargs -n 1 shellcheck -a --shell=sh -x'
 alias list-go-pkg='ls -1A "/go/bin"'
 alias build-app='/usr/local/bin/build-app.sh'
